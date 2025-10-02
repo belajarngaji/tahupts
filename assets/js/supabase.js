de5a8067b19988a1b5db4621c33c1a169caa987a
belajarngaji <ffarid.5213@gmail.com>
2025-10-02 15:23:41 +0700

    Create supabase.js

---
+++ assets/js/supabase.js
@@ -0,0 +1,9 @@
+// Integrasi Supabase Client
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+// ⚠️ Ganti dengan project Supabase kamu
+const SUPABASE_URL = "https://vrpdotzqyesnhewynxta.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZycGRvdHpxeWVzbmhld3lueHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTMyMzQsImV4cCI6MjA3MTg4OTIzNH0.2FGkxnEbD_L2VOu6mxnTcDK7l3Ncekwe425riYImpWI"
+
+// Export Supabase Client untuk dipakai di file lain
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
