85b7c9b9b44a7bca3e5dddc48a2175b63544db64
belajarngaji <ffarid.5213@gmail.com>
2025-10-02 17:25:09 +0700

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,9 +1,11 @@
+// supabase.js
+
 // Integrasi Supabase Client
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-// ⚠️ Ganti dengan project Supabase kamu
+// ⚠️ Ganti dengan project Supabase URL dan KEY Anda
 const SUPABASE_URL = "https://vrpdotzqyesnhewynxta.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZycGRvdHpxeWVzbmhld3lueHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTMyMzQsImV4cCI6MjA3MTg4OTIzNH0.2FGkxnEbD_L2VOu6mxnTcDK7l3Ncekwe425riYImpWI"
 
-// Export Supabase Client untuk dipakai di file lain
+// Export Supabase Client agar bisa di-import oleh form.js
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
